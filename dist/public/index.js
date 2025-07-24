44126cc5f05ac63f3a4e6248bbd67265698c7a1d
Joax <joaxdev9@gmail.com>
2025-07-24 06:25:01 -0300

    Compiled the TypeScript file created untill now

--- dist/public/index.js
+++ dist/public/index.js
@@ -1,3 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+console.log;
+export {};
 //# sourceMappingURL=index.js.map
--- dist/source/server.js
+++ dist/source/server.js
@@ -1,3 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import express from 'express';
+const app = express();
+const PORT = 5001;
+app.listen(PORT, `Your listening to port ${PORT}`);
 //# sourceMappingURL=server.js.map
