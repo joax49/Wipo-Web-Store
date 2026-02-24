cef01a05abe0d4025678c888166082bca37b2e07
Joax49 <joaxdev9@gmail.com>
2026-02-24 18:55:12 -0300

    Added support for the product type to be null when changing prices

--- dist/source/database/editPrice.js
+++ dist/source/database/editPrice.js
@@ -1,8 +1,16 @@
 import { pool } from "./main.js";
 export async function editPrices(oldFloorPrice, oldRoofPrice, newPrice, type) {
-    await pool.query(`
+    if (typeof type === "string") {
+        await pool.query(`
         UPDATE products SET price = $1
         WHERE price > $2 AND price < $3 AND type = $4
         `, [newPrice, oldFloorPrice, oldRoofPrice, type]);
+    }
+    else {
+        await pool.query(`
+        UPDATE products SET price = $1
+        WHERE price > $2 AND price < $3
+        `, [newPrice, oldFloorPrice, oldRoofPrice]);
+    }
 }
 //# sourceMappingURL=editPrice.js.map
--- src/source/database/editPrice.ts
+++ src/source/database/editPrice.ts
@@ -1,8 +1,17 @@
 import { pool } from "./main.js";
 
-export async function editPrices(oldFloorPrice:number, oldRoofPrice:number, newPrice:number, type:string) {
-    await pool.query(`
+export async function editPrices(oldFloorPrice:number, oldRoofPrice:number, newPrice:number, type:string | null) {
+    if(typeof type === "string") {
+        await pool.query(`
         UPDATE products SET price = $1
         WHERE price > $2 AND price < $3 AND type = $4
         `, [newPrice, oldFloorPrice, oldRoofPrice, type])
+    }
+
+    else {
+        await pool.query(`
+        UPDATE products SET price = $1
+        WHERE price > $2 AND price < $3
+        `, [newPrice, oldFloorPrice, oldRoofPrice])
+    }
 }
