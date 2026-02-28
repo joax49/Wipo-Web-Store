5d596170d275c1517e850083d4b8a88bee859ddf
Joax49 <joaxdev9@gmail.com>
2026-02-28 09:35:36 -0300

    Added the controller for getting the sales from the backend

--- src/source/controllers/getSalesController.ts
+++ src/source/controllers/getSalesController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import { getSales } from "../database/getSales.js";
 
 export async function getSalesController(req: Request, res: Response) {
-    
+
+    const allSales = getSales();
+    return res.status(201).json({"sales":allSales})
 }
